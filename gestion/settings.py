76fe5a73e8b300d71f23d0b8e748331a03e9780c
Your Name <you@example.com>
2024-08-01 17:09:57 -0700

    pussing to render

--- gestion/settings.py
+++ gestion/settings.py
@@ -36,7 +36,7 @@ SECRET_KEY = 'django-insecure-^6h2$14jel!4-*k6(68b%4!h5dc+p+v#3-9x_s)^bvfk-uwvin
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost','*']
 
 
 # Application definition
@@ -86,22 +86,22 @@ WSGI_APPLICATION = 'gestion.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'gestion',
-        'HOST' : '127.0.0.1',
-        'USER': 'root',
-        'PASSWORD':'',
-        'PORT':  3306
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'gestion',
+#         'HOST' : '127.0.0.1',
+#         'USER': 'root',
+#         'PASSWORD':'',
+#         'PORT':  3306
+#     }
+# }
 
 
 # Password validation
